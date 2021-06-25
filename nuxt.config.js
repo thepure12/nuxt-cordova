b96be25bd53a565e6902b670c6a8456ccc755b76
TJ <renningert@gmail.com>
2021-06-25 16:40:02 -0500

    removed old server

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: "https://purefit.life"
+    baseURL: "https://localhost:80"
   },
   auth: {
     strategies: {
